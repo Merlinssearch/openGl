07686a32b55e0bb32fcf8e22dac63c168050663b
maxim <merlinssearch@gmail.com>
2026-06-30 10:07:57 +0200

    Learned : Simple perspective rotation

--- shader/vertexshader.glsl
+++ shader/vertexshader.glsl
@@ -1,20 +1,13 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;   // the position variable has attribute position 0
 layout (location = 1) in vec3 aColor; // the color variable has attribute position 1
-
-uniform float time;
-// uniform vec4 timeColor;
+  
+uniform float time; 
 out vec3 ourColor; // output a color to the fragment shader
 
 void main()
 {   
-    gl_Position = vec4(aPos, 1.0);
-    ourColor = aColor;
-    // if (gl_VertexID == 0 ) {
-    //   ourColor = aColor * vec3((sin(time*2)+1)/2,(-sin(time*2)+1)/2,sin(time)*0);
-    // } else if (gl_VertexID == 1) {
-    //   ourColor = aColor * vec3((sin(time*2)+1)/2,(-sin(time*2)+1)/2,sin(time)*0);
-    // }else if (gl_VertexID == 2) {
-    //   ourColor = aColor * vec3((sin(time*2)+1)/2,(-sin(time*2)+1)/2,sin(time)*0);
-    // }
-}          
+    vec4 transform = vec4(cos(time) * aPos.x + sin(time) * aPos.z, aPos.y , -sin(time) * aPos.x + cos(time) * aPos.z , 1.0) ;
+    gl_Position = transform;
+    ourColor = aColor; // set ourColor to the input color we got from the vertex data
+}               
